b4ca10f7f9a97c0373e12016de124e8f0f914413
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-12-27 09:54:13 -0300

    initial backend config

---
+++ backend/src/server/index.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+export { server }
